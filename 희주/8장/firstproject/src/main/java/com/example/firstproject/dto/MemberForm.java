431281c9f614e57d1f6cdc45ca6231dcf80b8e60
KIM HEEJU <hillarygmlwn02@gmail.com>
2025-05-17 03:21:42 +0900

    Add files via upload

---
+++ 희주/8장/firstproject/src/main/java/com/example/firstproject/dto/MemberForm.java
@@ -0,0 +1,20 @@
+package com.example.firstproject.dto;
+
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+
+public class MemberForm {
+    private String email;
+    private String password;
+    private Long id;
+
+
+    public Member toEntity() {
+        return new Member(id, email, password);
+    }
+}
